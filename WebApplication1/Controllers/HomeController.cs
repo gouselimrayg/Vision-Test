9f6fe1aceb93f15955b515418533cf6e8d35c167
sainaveenpujari <sanaveen100@gmail.com>
2021-01-31 14:17:34 +0530

    First commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<Country> country = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://restcountries.eu/rest/v2/all");
+                //HTTP Get
+                var postTask = client.GetAsync("https://restcountries.eu/rest/v2/all");
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Country>>();
+                    readTask.Wait();
+                    country = readTask.Result;
+                }
+            }
+
+            return View(country);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Country
+    {
+        public string name { get; set; }
+        public string capital { get; set; }
+        public string[] callingCodes {set;get;}
+        public List<Currency> currencies { get; set; }
+    }
+}
